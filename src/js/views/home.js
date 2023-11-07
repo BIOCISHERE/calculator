c6b689346455db535a064abb3a3922c79afb7a41
Benjamin Ocaranza <119523448+BIOCISHERE@users.noreply.github.com>
2023-11-07 19:17:31 +0000

    basic interface done

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,43 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container">
+		<div className="row">
+			<div className="col border border-primary">
+				<div className="border border-warning w-1oo">aqui ira el input</div>
+				<div className="row">
+					<div className="col">
+						<button className="calculatorBTN">7</button>
+						<button className="calculatorBTN">8</button>
+						<button className="calculatorBTN">9</button>
+						<button className="calculatorBTN">C</button>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col">
+						<button className="calculatorBTN">4</button>
+						<button className="calculatorBTN">5</button>
+						<button className="calculatorBTN">6</button>
+						<button className="calculatorBTN">X</button>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col">
+						<button className="calculatorBTN">1</button>
+						<button className="calculatorBTN">2</button>
+						<button className="calculatorBTN">3</button>
+						<button className="calculatorBTN">-</button>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col">
+						<button className="calculatorBTN">0</button>
+						<button className="calculatorBTN">.</button>
+						<button className="calculatorBTN">=</button>
+						<button className="calculatorBTN">+</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
