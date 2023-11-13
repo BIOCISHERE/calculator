38b36abaaf55206eef85b44917163fee32cbda22
Benjamin Ocaranza <119523448+BIOCISHERE@users.noreply.github.com>
2023-11-13 23:50:56 +0000

    working in the numbers onclick

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,52 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="container-fluid">
-		<div className="row calculatorShape m-auto">
-			<div className="col">
-				<div className="border border-dark calculatorInput">
-					<h1>Aqui ira el input</h1>
-				</div>
-				<div className="row">
-					<div className="col">
-						<button className="calculatorBTN">7</button>
-						<button className="calculatorBTN">8</button>
-						<button className="calculatorBTN">9</button>
-						<button className="calculatorBTN">÷</button>
+export const Home = () => {
+	const [isFirstNumber, setIsFirstNumber] = useState("0")
+	const [isSecondNumber, setIsSecondNumber] = useState("0")
+
+	const numbersOnClick = (numb) => {
+		if (isFirstNumber == "0") {
+			let recivingNmbr = numb
+			let nmbrToString = recivingNmbr.toString()
+
+			setIsFirstNumber(nmbrToString)
+		} else {
+			let recivingNmbr = numb
+			let nmbrToString = recivingNmbr.toString()
+			let updateNumber = isFirstNumber.concat(nmbrToString)
+			
+			setIsFirstNumber(updateNumber)
+		}
+	}
+
+	return (
+		<div className="container-fluid">
+			<div className="row calculatorShape m-auto">
+				<div className="col">
+					<div className="border border-dark calculatorInput">
+						<h1>{isFirstNumber}</h1>
 					</div>
-				</div>
-				<div className="row">
-					<div className="col">
-						<button className="calculatorBTN">4</button>
-						<button className="calculatorBTN">5</button>
-						<button className="calculatorBTN">6</button>
-						<button className="calculatorBTN">X</button>
+					<div className="row">
+						<div className="col">
+							<button className="calculatorBTN" onClick={() => numbersOnClick(7)}>7</button>
+							<button className="calculatorBTN">8</button>
+							<button className="calculatorBTN">9</button>
+							<button className="calculatorBTN">÷</button>
+						</div>
 					</div>
-				</div>
-				<div className="row">
-					<div className="col">
-						<button className="calculatorBTN">1</button>
-						<button className="calculatorBTN">2</button>
-						<button className="calculatorBTN">3</button>
-						<button className="calculatorBTN">-</button>
+					<div className="row">
+						<div className="col">
+							<button className="calculatorBTN">4</button>
+							<button className="calculatorBTN">5</button>
+							<button className="calculatorBTN">6</button>
+							<button className="calculatorBTN">X</button>
+						</div>
 					</div>
-				</div>
-				<div className="row">
-					<div className="col">
-						<button className="calculatorBTN">0</button>
-						<button className="calculatorBTN">.</button>
-						<button className="calculatorBTN">=</button>
-						<button className="calculatorBTN">+</button>
+					<div className="row">
+						<div className="col">
+							<button className="calculatorBTN">1</button>
+							<button className="calculatorBTN">2</button>
+							<button className="calculatorBTN">3</button>
+							<button className="calculatorBTN">-</button>
+						</div>
 					</div>
-				</div>
-				<div className="row">
-					<div className="col">
-						<button className="clearBTN">C</button>
+					<div className="row">
+						<div className="col">
+							<button className="calculatorBTN">0</button>
+							<button className="calculatorBTN">.</button>
+							<button className="calculatorBTN">=</button>
+							<button className="calculatorBTN">+</button>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col">
+							<button className="clearBTN">Clear</button>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-);
+	)
+};
