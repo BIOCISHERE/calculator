7c5eb64df74defec9ee92f2744a6fa4b68fa799c
Benjamin Ocaranza <119523448+BIOCISHERE@users.noreply.github.com>
2023-11-18 00:29:34 +0000

    Edited the navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaCalculator } from "react-icons/fa";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1"><FaCalculator className="m-1" />Basic Calculator</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
